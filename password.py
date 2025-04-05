434b87c712364c5189047ddd4dca0716ccb42162
Akhila Mokkapati <68263426+akhilamokkapati@users.noreply.github.com>
2025-04-05 13:20:01 +0800

    Add files via upload

---
+++ password.py
@@ -0,0 +1,18 @@
+#password generator
+import random                       
+
+# defining a function to randomly generate the password  
+def generate_pw(length):    
+
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()"  
+    str = ""  
+
+    for i in range(length):  
+        str = str + random.choice(characters)  
+
+    return str
+
+
+length = int(input("Enter the length of the desired password: "))
+str = generate_pw(length)  
+print("Generated Password:", str)
